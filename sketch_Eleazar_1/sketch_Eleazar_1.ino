03e8e7c8ce48f84e841d716ad6cfd20360d97680
JonnyJaccob <57542964+JonnyJaccob@users.noreply.github.com>
2023-05-26 23:10:35 -0500

    cambio en .ino

--- sketch_Eleazar_1/sketch_Eleazar_1.ino
+++ sketch_Eleazar_1/sketch_Eleazar_1.ino
@@ -1,19 +1,21 @@
 #include <SoftwareSerial.h>
 
-const int motorPinAbrir = 2;
-const int motorPinCerrar = 3;
+const int motorPinCerrar = 2;
+const int motorPinAbrir = 8;
 bool Abierto = true;
 bool Cerrado = false;
 int umbralMax = 32;  // Umbral máximo de temperatura
-int umbralMin = 20;  // Umbral mínimo de temperatura
+int umbralMin = 26;  // Umbral mínimo de temperatura
 
 SoftwareSerial serial(10, 11);  // RX, TX (puertos 10 y 11 del Arduino)
 
 void setup() {
   Serial.begin(9600);
   serial.begin(9600);  // Inicia la comunicación serie con la aplicación en C#
-  pinMode(motorPinAbrir, OUTPUT);
   pinMode(motorPinCerrar, OUTPUT);
+  digitalWrite(motorPinCerrar, HIGH);
+  pinMode(motorPinAbrir, OUTPUT);
+  digitalWrite(motorPinAbrir, HIGH);
 }
 
 void loop() {
@@ -26,23 +28,23 @@ void loop() {
 
   serial.print(celsius);  // Envía los datos de temperatura a través de la comunicación serie
 
-  if (celsius >= umbralMax && Abierto) { //Variable que cambia desde c#
-    analogWrite(motorPinAbrir, 150);
+  if (celsius >= umbralMax && Abierto) { // Variable que cambia desde C#
+    digitalWrite(motorPinCerrar, LOW);
     delay(1000);
-    analogWrite(motorPinAbrir, 0); 
+    digitalWrite(motorPinCerrar, HIGH);
     Serial.println("Se cerro");
     Abierto = false;
     Cerrado = true;
   }
 
-  if (celsius <= umbralMin && Cerrado) { //umbralMin que cambie desde c#
-    analogWrite(motorPinAbrir, 150);
-    delay(1000);                    
-    analogWrite(motorPinAbrir, 0);
+  if (celsius <= umbralMin && Cerrado) { // UmbralMin que cambie desde C#
+    digitalWrite(motorPinAbrir, LOW);
+    delay(3000);
+    digitalWrite(motorPinAbrir, HIGH);
     Serial.println("Se abrio");
     Abierto = true;
     Cerrado = false;
   }
 
-  delay(2000);
+  delay(4000);
 }
